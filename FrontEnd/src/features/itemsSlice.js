5e349aedd72894b60255254bb77a1515e1519046
Sami <52921619+sbagh@users.noreply.github.com>
2022-12-18 14:38:13 -0500

    Update itemsSlice.js

--- FrontEnd/src/features/itemsSlice.js
+++ FrontEnd/src/features/itemsSlice.js
@@ -1,30 +1,25 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios'
+import axios from "axios";
 
 const initialState = {
    things: [],
    status: null,
 };
 
-export const productsFetch = createAsyncThunk(
-    "items/itemsFetch",
-    async()=> {
-        const response = await axios.get("http://localhost:5100/items");
-        return response?.data
-    }
-) 
-
+export const itemsFetch = createAsyncThunk(
+    "items/itemsFetch", 
+    async () => {
+        const response = await axios.get("http://localhost:5100/items")
+        return response?.data;
+});
 
 const itemsSlice = createSlice({
    name: "items",
    initialState,
    reducers: {},
    extraRedcuers: {
-    [itemsFetch.pending]: (state, action) => {
-
-    }
-
-   }
+      [itemsFetch.pending]: (state, action) => {},
+   },
 });
 
 export default itemsSlice.reducer;
